715280970e14e84e5138d0fb33587e4044af7de3
tinker <thom@hometreelab.com>
2019-12-16 12:29:33 +1000

    Update codebase, move to GH, remove unneeded SFX stuff

--- s3b0.py
+++ s3b0.py
@@ -6,20 +6,11 @@ import asyncio
 from os import environ
 from random import randint
 
-# Todo
-# update the voice_client handling to account for bot being active on multiple servers at the same time
-# add auto population of the sfx library based on the filesystem
-# add command to update sfx population (see above) while running (i.e. without restart of bot)
-
 logging.basicConfig(level=logging.INFO)
 
 client = discord.Client()
 discord.opus.load_opus
-voice_client = None
-roll_command = "/roll"
-voice_join_command = "/joinme"
-voice_leave_command = "/nini"
-voice_play_command = "/sfx"
+roll_command = "/swoll"
 img_base="https://workshop.hometreelab.com/tinker-public/s3b0/raw/master/images/alpha/"
 
 Advantage="Advantage"
@@ -40,10 +31,6 @@ Red=0xe74c3c
 DarkRed=0x992d22
 LightPip = u"\u26AA"
 DarkPip = u"\u26AB"
-
-sfxDict = {
-    "kit": 10
-}
 
 LastPlayingIndex=-1
 PlayingQuotes = {
@@ -369,24 +356,12 @@ def rollDie(min=1, max=6):
     result = randint(min,max)
     return result
 
-def playSFX(command):
-    global voice_client
-    if command in sfxDict:
-        sfxItems = sfxDict[command]
-        result = randint(1,sfxItems)
-        player = voice_client.create_ffmpeg_player('sfx/' + command + '/' + str(result) + '.mp3')
-        player.start()
-        return None
-    else:
-        return "No SFX found for " + command
-
 async def cyclePlaying():
     global LastPlayingIndex
     playing=PlayingQuotes[randint(1,len(PlayingQuotes))]
     while playing == LastPlayingIndex:
         playing=PlayingQuotes[randint(1,len(PlayingQuotes))]
     LastPlayingIndex=playing
-    print("Playing " + playing)
     await client.change_presence(game=discord.Game(name=playing))
     await asyncio.sleep(randint(60,600))
 
@@ -398,12 +373,11 @@ async def on_ready():
         
 @client.event
 async def on_message(message):
-    global voice_client
     if message.author == client.user:
         return
     if message.content == roll_command:
         msg = """```
-/roll [[number=1][die type]]...
+/swoll [[number=1][die type]]...
 
 Die Types:
     a: Ability
@@ -417,9 +391,9 @@ Die Types:
     h: Hundred (d100) (can't be chained)
 
 Example:
-    /roll 3a1p2c1b2s
-    /roll 3f
-    /roll h
+    /swoll 3a1p2c1b2s
+    /swoll 3f
+    /swoll h
 ```"""
         await client.send_message(message.channel, msg)
         return
@@ -436,39 +410,5 @@ Example:
                 em.description=None
             await client.send_message(message.channel, embed=em)
     
-    # voice stuff
-    if message.content.startswith(voice_play_command):
-        if voice_client:
-            msg = playSFX(message.content[len(voice_play_command)+1:])
-            if msg:
-                await client.send_message(message.channel, msg)
-        else:
-            msg = """I need to be invited into your active voice channel first!
-Enter `/joinme` _after_ you've joined a voice channel"""
-            await client.send_message(message.channel, msg)
-    if message.content.startswith(voice_leave_command):
-        if voice_client:
-            await voice_client.disconnect()
-            voice_client = None
-            msg = "Nini!"
-            await client.send_message(message.channel, msg)
-        else:
-            msg = "I'm not in any voice channels!"
-            await client.send_message(message.channel, msg)
-    if message.content.startswith(voice_join_command):
-        if voice_client:
-            await voice_client.disconnect()
-            voice_client = None
-        author = message.author
-        channel = author.voice_channel
-        if channel:
-            voice_client = await client.join_voice_channel(channel)
-            msg = "I'm in!"
-            await client.send_message(message.channel, msg)
-        else:
-            msg = """You're not in any voice channels on this server!
-Enter `/joinme` _after_ you've joined a voice channel"""
-            await client.send_message(message.channel, msg)
-
 token=environ['S3B0_TOKEN']
 client.run(token)
